e6ff3d848c90eb59651d6000acf6a651de48c0ec
ohidul alam <mdtasib442@gmali.com>
2021-02-05 18:13:25 +0600

    practice

--- all.js
+++ all.js
@@ -331,3 +331,33 @@ let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 //     }
 // }
 // console.log(new Person('tasib', 19, 'bangladesh'));
+
+// async await how to use it for async call (1)
+// 1
+// function loadData() {
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//         .then(res => res.json())
+//         .then(data => displayData(data))
+// }
+// loadData();
+
+// 2
+// async function loadData() {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = await response.json();
+//     return data;
+// }
+
+// loadData().then(data => {
+//     displayData(data)
+// })
+
+// function displayData(data) {
+//     const ul = document.getElementById('list');
+//     for (let i = 0; i < data.length; i++) {
+//         const users = data[i];
+//         const li = document.createElement('li');
+//         li.innerText = users.name;
+//         ul.appendChild(li);
+//     }
+// }
