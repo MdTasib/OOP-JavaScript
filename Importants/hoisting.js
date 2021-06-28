bb408585d00f22f9ebf22f420fa346117ad92dad
MdTasib <ohidulalam442@gmail.com>
2021-06-28 10:21:38 +0600

    hoisting

---
+++ Importants/hoisting.js
@@ -0,0 +1,20 @@
+// Hoisting
+// var vs let, const
+// function
+
+/*
+// var
+// 1
+console.log(b);
+var b = 'Ball';
+
+// 2
+var a;
+console.log(a);
+a = 'Bangladesh';
+console.log(a);
+*/
+
+// let
+console.log(c);
+let c = 'cat';
