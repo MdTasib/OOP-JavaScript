f35653f5a68b67443b9ec2fe618855ec5540bddd
MdTasib <ohidulalam442@gmail.com>
2021-07-03 23:13:21 +0600

    class

--- Importants/classes.js
+++ Importants/classes.js
@@ -1,6 +1,16 @@
-function Person(name, age) {
+// Prototype inheritance
+
+function Person(name, age) { // parent class
     this.name = name;
     this.age = age;
+}
+
+function Developer(name, age, type, country) { // child class
+    Person.call(this);
+    this.name = name;
+    this.age = age;
+    this.type = type;
+    this.country = country;
 }
 
 Person.prototype = {
@@ -9,11 +19,16 @@ Person.prototype = {
     }
 }
 
-const tasib = new Person('Tasib', 19);
-console.log(tasib);
-tasib.eat()
-const rafi = new Person('Rafi', 22);
-console.log(rafi);
+Developer.prototype = Object.create(Person.prototype);
+Developer.prototype.constructor = Developer;
+Developer.prototype.play = function () {
+    console.log(`${this.name} is playing`);
+}
+
+const tasib = new Developer('Tasib', 19, 'Front End', 'Bangladesh');
+console.log(tasib.eat());
+console.log(tasib.play());
+
 
 /*
 // prototype chine
@@ -26,3 +41,33 @@ Object.prototype.tasib = function () {
 let p = {};
 p.tasib();
 */
+
+// Convert Class
+class Human {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    eat() {
+        console.log(`${this.name} is eating`);
+    }
+}
+
+class Programmer extends Human {
+    constructor(name, age, type, country) {
+        super(name, age);
+        this.name = name;
+        this.age = age;
+        this.type = type;
+        this.country = country;
+    }
+
+    play() {
+        console.log(`${this.name} is playing`);
+    }
+}
+
+const rafi = new Programmer('Raft', 22, 'MERN Stack', 'Bangladesh');
+console.log(rafi.name);
+rafi.eat();
