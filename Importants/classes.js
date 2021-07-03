b6768cadc6ef5aaced705de6be50ccb82662b61c
MdTasib <ohidulalam442@gmail.com>
2021-07-03 22:38:48 +0600

    class

--- Importants/classes.js
+++ Importants/classes.js
@@ -14,3 +14,15 @@ console.log(tasib);
 tasib.eat()
 const rafi = new Person('Rafi', 22);
 console.log(rafi);
+
+/*
+// prototype chine
+let f = function Human() { };
+
+Object.prototype.tasib = function () {
+    console.log('i am tasib');
+}
+
+let p = {};
+p.tasib();
+*/
