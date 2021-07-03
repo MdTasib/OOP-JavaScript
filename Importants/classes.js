2052e02489c0af1890e2d195aa6274eb191a8277
MdTasib <ohidulalam442@gmail.com>
2021-07-03 17:50:29 +0600

    class

--- Importants/classes.js
+++ Importants/classes.js
@@ -3,4 +3,14 @@ function Person(name, age) {
     this.age = age;
 }
 
+Person.prototype = {
+    eat: function () {
+        console.log(`${this.name} is eating`);
+    }
+}
+
 const tasib = new Person('Tasib', 19);
+console.log(tasib);
+tasib.eat()
+const rafi = new Person('Rafi', 22);
+console.log(rafi);
